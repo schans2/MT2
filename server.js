937529f15438a2dbf9e8e6077af5e626fdba4afb
schans2 <schans2@rpi.edu>
2022-01-31 11:48:15 -0500

    Barebones Skeleton Things
    Just establishing some files here

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const httpServer = require("http");
+const bodyParser = require("body-parser");
+var app = express();
+var http = httpServer.createServer(app);
+
+// Initialize site files
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve main page
+app.get('/', function(req, res) { res.sendFile(__dirname + "/index.html"); });
+
+http.listen(3000, function() {
+  console.log("\x1b[32mServer running on localhost:3000");
+  console.log("\x1b[33mPress CTRL + C to shut down\x1b[0m");
+});
