9d5ed4a7322841fad83c097076f0a1491d2412ed
schans2 <schans2@rpi.edu>
2022-02-11 10:35:35 -0500

    Basic sound & volume incorporation

---
+++ client.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  alert("Hello world, jQuery connected!");
+
+  let saw = new Wad({source: "sawtooth", pitch: "C2", env: {hold: -1}});
+  let square = new Wad({source: "square", pitch: "G3", env: {hold: -1}});
+  let sine = new Wad({source: "sine", pitch: "Db3", env: {hold: -1}});
+  let triangle = new Wad({source: "square", pitch: "D#3", env: {hold: -1}});
+  let playStatus = "none";
+
+  $(".inst").click(function() {
+    if(playStatus !== this.name) {
+      eval(this.name).play();
+      $("#vol").val(eval(this.name).volume);
+      playStatus = this.name;
+    }
+    else {
+      eval(this.name).stop();
+      playStatus = "none";
+    }
+  });
+
+  $("#vol").on("input", function() {
+    eval(playStatus).setVolume($(this).val());
+  });
+});
