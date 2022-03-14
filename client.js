85eb608b5f0a646bb2de70e8f44df0201c145966
schans2 <schans2@rpi.edu>
2022-03-14 17:43:35 -0400

    Click and drag sequencing
    Still need to get the "click" part working lol

--- client.js
+++ client.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
   let sine = new Wad({source: "sine", pitch: "Db3", env: {hold: -1}});
   let triangle = new Wad({source: "square", pitch: "D#3", env: {hold: -1}});
   let playStatus = "none";
+  let drag = false;
+
+  $("body").mousedown(function() { drag = true; });
+
+  $("body").mouseup(function() { drag = false; });
 
   $(".inst").click(function() {
     if(playStatus !== this.name) {
@@ -21,6 +26,24 @@ $(document).ready(function() {
 
   $("#vol").on("input", function() {
     eval(playStatus).setVolume($(this).val());
+  });
+
+  $("#seqTable>div").mouseenter(function() {
+    if(drag) {
+      if($(this).hasClass("note")) {
+        $(this).removeClass("note");
+        $(this).css("backgroundColor", "white");
+      }
+      else {
+        // Multi-color descision tree thing here
+        // But for now...
+        $(this).addClass("note");
+        $(this).css("backgroundColor", "tomato");
+      }
+    }
+    // if($(this).hasClass("r12")) {
+
+    // }
   });
 
   new jBox("Modal", {
