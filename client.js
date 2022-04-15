488dda201ff268ad85bf899ddf1622c612f65ad9
schans2 <schans2@rpi.edu>
2022-04-15 12:01:59 -0400

    Multi-octave support, single sequence playback
    Still need multi-sequence playback

--- client.js
+++ client.js
@@ -38,20 +38,25 @@ $(document).ready(function() {
   function playSequence(tableArr) {
     // Tempo = 60 BPM for now (1/32nd note = 0.125s)
     // Process the 32 "ticks" per measure
+    let instrument = "";
     if(tableArr === "none") {
       tableArr = $(this).parent().parent().siblings().text();
-      switch (tableArr) {
+      switch(tableArr) {
         case "Saw Sequencing":
           tableArr = "sawSeq";
+          instrument = "saw";
           break;
         case "Square Sequencing":
           tableArr = "sqSeq";
+          instrument = "square";
           break;
         case "Sine Sequencing":
           tableArr = "sinSeq";
+          instrument = "sine";
           break;
         case "Triangle Sequencing":
           tableArr = "triSeq";
+          instrument = "triangle";
           break;
       }
     }
@@ -67,19 +72,59 @@ $(document).ready(function() {
     seqFound = [];
     function sequentialize() {
       setTimeout(() => {
+        let noteLen = 0;
+        let notePitch = "A4";
         // Find out what notes are triggering on this tick
         for(let i = 0; i < eval(tableArr).length; i++) {
           let nsRow = parseInt(eval(tableArr)[i].attr("class").split(/\s+/)[0].split('r')[1]);
+          let nsOct = eval(tableArr)[i].attr("class").split(/\s+/)[2].split('o')[1];
+          switch(nsRow) {
+            case 12:
+              notePitch = "C" + nsOct;
+              break;
+            case 11:
+              notePitch = "C#" + nsOct;
+              break;
+            case 10:
+              notePitch = "D" + nsOct;
+              break;
+            case 9:
+              notePitch = "D#" + nsOct;
+            case 8:
+              notePitch = "E" + nsOct;
+              break;
+            case 7:
+              notePitch = "F" + nsOct;
+              break;
+            case 6:
+              notePitch = "F#" + nsOct;
+              break;
+            case 5:
+              notePitch = "G" + nsOct;
+              break;
+            case 4:
+              notePitch = "G#" + nsOct;
+              break;
+            case 3:
+              notePitch = "A" + nsOct;
+              break;
+            case 2:
+              notePitch = "A#" + nsOct;
+              break;
+            case 1:
+              notePitch = "B" + nsOct;
+              break;
+          }
           if(eval(tableArr)[i].hasClass("c" + ctr) &&
             // https://www.codegrepper.com/code-examples/javascript/js+2d+array+includes
             !seqFound.some(row => JSON.stringify(row) === JSON.stringify([nsRow, ctr]))
           ) {
             // For each triggered note, search ahead to see how long it should be sustained
-            let noteLen = 0.125;
+            noteLen = 0.125;
             // Proxy note object
             let scanNote = eval(tableArr)[i].clone()
               .removeClass("c" + ctr).addClass("c" + (ctr + 1))
-              .removeClass("o1").addClass("o1");
+              .removeClass("o" + nsOct).addClass("o" + nsOct);
             for(let j = i; j < eval(tableArr).length; j++) {
               // See if "one tick ahead" exists
               if(eval(tableArr)[j].attr("class") === scanNote.attr("class")) {
@@ -87,11 +132,21 @@ $(document).ready(function() {
                 let prevNum = parseInt(scanNote.attr("class").split(/\s+/)[1].split('c')[1]);
                 seqFound.push([parseInt(scanNote.attr("class").split(/\s+/)[0].split('r')[1]), prevNum]);
                 scanNote.removeClass("c" + prevNum).addClass("c" + (prevNum + 1))
-                  .removeClass("o1").addClass("o1");
+                  .removeClass("o" + nsOct).addClass("o" + nsOct);
                 noteLen += 0.125;
               }
             }
             console.log(noteLen);
+            eval(instrument).play({
+              pitch: notePitch,
+              env: {
+                attack: 0,
+                decay: 0,
+                sustain: 0.2,
+                hold: noteLen,
+                release: 0
+              }
+            });
           }
         }
         console.log(ctr);
@@ -109,7 +164,7 @@ $(document).ready(function() {
 
     $(".seqTable>div").mousedown(function() {
       let tableArr = $(this).parents().eq(2).siblings().text();
-      switch (tableArr) {
+      switch(tableArr) {
         case "Saw Sequencing":
           tableArr = "sawSeq";
           break;
@@ -124,9 +179,9 @@ $(document).ready(function() {
           break;
       }
       drag = true;
-      // Remember to implement proper multi-octave support
-      if($(this).hasClass("o1")) {
-        $(this).removeClass("o1");
+      let classArr = $(this).attr("class").split(/\s+/);
+      if(classArr.length === 3) {
+        $(this).removeClass(classArr[2]);
         eval(tableArr).splice($.inArray($(this), eval(tableArr)), 1);
         $(this).css("backgroundColor", "white");
       }
@@ -144,7 +199,7 @@ $(document).ready(function() {
 
     $(".seqTable>div").mouseenter(function() {
       let tableArr = $(this).parents().eq(2).siblings().text();
-      switch (tableArr) {
+      switch(tableArr) {
         case "Saw Sequencing":
           tableArr = "sawSeq";
           break;
@@ -159,9 +214,9 @@ $(document).ready(function() {
           break;
       }
       if(drag) {
-        // Remember to implement proper multi-octave support
-        if($(this).hasClass("o1")) {
-          $(this).removeClass("o1");
+        let classArr = $(this).attr("class").split(/\s+/);
+        if(classArr.length === 3) {
+          $(this).removeClass(classArr[2]);
           eval(tableArr).splice($.inArray($(this), eval(tableArr)), 1);
           $(this).css("backgroundColor", "white");
         }
